855015b39f6b05b1d9a683628fbf2815faa96f41
caocuongccc <caocuongccc@gmail.com>
2016-05-26 07:29:35 +0700

    remove js

--- js/application.js
+++ js/application.js
@@ -1,13 +1,1 @@
-console.log('aaaaaaaaaaaaaaaaaaaaaaaaa');
-jQuery(document).ready(function() {
 
-       // STICKY HEADER
-       jQuery(window).scroll(function() {
-           if (jQuery(this).scrollTop() > 0){ // 75 is the height of contact-container
-               jQuery('.l-header').removeClass("scroll");
-           }
-           else{
-               jQuery('.l-header').addClass("scroll");
-           }
-       });
-});
